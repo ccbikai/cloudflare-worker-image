a4331fd8b0ea3c31dafe41b1bb3745a434875261
ccbikai <ccbikai@gmail.com>
2023-11-24 19:25:10 +0800

    feat: cloudflare-worker-image

---
+++ README.md
@@ -0,0 +1,78 @@
+# Cloudflare Worker Image
+
+使用 Cloudflare Worker 处理图片, 依赖 Photon(依赖 patch-package)、 Squash WebAssembly。
+
+---
+
+![Cloudflare Workers](https://img.shields.io/badge/Cloudflare-F69652?style=flat&logo=cloudflare&logoColor=white)
+![GitHub License](https://img.shields.io/github/license/ccbikai/cloudflare-worker-image)
+![GitHub Repo stars](https://img.shields.io/github/stars/ccbikai/cloudflare-worker-image)
+
+## 支持特性
+
+1. 支持 PNG、JPEG、BMP、ICO、TIFF 格式图片处理
+2. 可输出 JPEG、PNG、WEBP 格式图片，默认输出 WEBP 格式图片
+3. 支持管道操作，可以执行多个操作
+4. 支持 Cloudflare 缓存
+5. 支持图片地址白名单，防滥用
+6. 异常降级，如果处理失败返回原图（异常场景不缓存）
+
+## 部署方式
+
+```sh
+# patch 功能依赖 pnpm, 如果不使用 pnpm, 需要自己处理 patch-package https://www.npmjs.com/package/patch-package
+npm i -g pnpm
+
+# 克隆此项目
+git clone https://github.com/ccbikai/cloudflare-worker-image.git
+cd cloudflare-worker-image
+
+# 安装依赖
+pnpm install
+
+# 修改白名单配置，改为图片域名或者留空不限制图片地址
+vi wrangler.toml # WHITE_LIST
+
+# 发布
+npm run deploy
+```
+
+## 使用方式
+
+修改域名和参数即可使用, 参考：<https://image.miantiao.me/?url=https%3A%2F%2Fstatic.miantiao.me%2Fshare%2FMTyerw%2Fbanner-2048.jpeg&action=resize!830,400,2>
+
+### 参数说明
+
+url:
+> 原图地址，需要使用 encodeURIComponent 编码
+
+action:
+> 操作指令, 支持 [Photon](https://docs.rs/photon-rs/latest/photon_rs/) 各种操作指令，指令与参数直接使用`!`分割，参考 `resize!830,400,2`
+>
+> 支持管道操作，多个操作指令使用`|`分割，参考 `resize!830,400,2|watermark!https%3A%2F%2Fstatic.miantiao.me%2Fshare%2F6qIq4w%2FFhSUzU.png,10,10`
+>
+> 如果参数中有 URL 或其他特殊字符，需要使用 encodeURIComponent 编码 URL 和 特殊字符
+
+format:
+> 输出图片格式，支持：`jpg,webp,png`，可选，默认 webp
+
+quality:
+> 图片质量，1-100 只有 webp 和 jpg 格式支持，可选，默认 99
+
+## 演示
+
+![resize](https://image.miantiao.me/?url=https%3A%2F%2Fstatic.miantiao.me%2Fshare%2FMTyerw%2Fbanner-2048.jpeg&action=resize!830,400,2)
+
+由于 Github 会缓存图片，请前往我博客查看真实示例。
+
+<http://chi.miantiao.me/post/cloudflare-worker-image/>
+
+## 致谢
+
+- [Cloudflare](https://www.cloudflare.com)
+- [photon](https://github.com/silvia-odwyer/photon)
+- [jSquash](https://github.com/jamsinclair/jSquash)
+
+---
+
+[![Buy Me A Coffee](https://static.miantiao.me/share/0WmsVP/CcmGr8.png)](https://www.buymeacoffee.com/ccbikai)
---
+++ src/index.js
@@ -0,0 +1,140 @@
+import queryString from 'query-string';
+
+import * as photon from '@silvia-odwyer/photon';
+import * as PHOTON_WASM_JS from '../node_modules/@silvia-odwyer/photon/photon_rs_bg.js';
+import PHOTON_WASM from '../node_modules/@silvia-odwyer/photon/photon_rs_bg.wasm';
+
+import encodeWebp, { init as initWebpWasm } from '@jsquash/webp/encode';
+import encodeJpeg, { init as initJpegWasm } from '@jsquash/jpeg/encode';
+import encodePng, { init as initPngWasm } from '@jsquash/png/encode';
+
+import WEBP_ENC_WASM from '../node_modules/@jsquash/webp/codec/enc/webp_enc.wasm';
+import JPEG_ENC_WASM from '../node_modules/@jsquash/jpeg/codec/enc/mozjpeg_enc.wasm';
+import PNG_ENC_WASM from '../node_modules/@jsquash/png/codec/squoosh_png_bg.wasm';
+
+const OUTPUT_FORMATS = {
+	jpeg: 'image/jpeg',
+	jpg: 'image/jpeg',
+	png: 'image/png',
+	webp: 'image/webp',
+};
+
+const multipleImageMode = ['watermark', 'blend'];
+
+const inWhiteList = (env, url) => {
+	const imageUrl = new URL(url);
+	const whiteList = env.WHITE_LIST ? env.WHITE_LIST.split(',') : [];
+	return !(whiteList.length && !whiteList.find((hostname) => imageUrl.hostname.endsWith(hostname)));
+};
+
+const processImage = async (env, request, inputImage, pipeAction) => {
+	const [ action, options ] = pipeAction.split('!')
+	const params = options.split(',');
+	if (multipleImageMode.includes(action)) {
+		const image2 = params.shift(); // 是否需要 decodeURIComponent ?
+		if (image2 && inWhiteList(env, image2)) {
+			const image2Res = await fetch(image2, {headers: request.headers});
+			if (image2Res.ok) {
+				const inputImage2 = photon.PhotonImage.new_from_byteslice(new Uint8Array(await image2Res.arrayBuffer()));
+				// 多图处理是处理原图
+				photon[action](inputImage, inputImage2, ...params);
+				return inputImage; // 多图模式返回第一张图
+			}
+		}
+	} else {
+		return photon[action](inputImage, ...params);
+	}
+};
+
+export default {
+	async fetch(request, env, context) {
+		// 读取缓存
+		const cache = caches.default;
+		const hasCache = await cache.match(request);
+		if (hasCache) {
+			return hasCache;
+		}
+
+		// 入参提取与校验
+		const query = queryString.parse(new URL(request.url).search);
+		const { url = '', action = '', format = 'webp', quality = 99 } = query;
+		console.log('params:', url, action, format, quality);
+
+		if (!url) {
+			return new Response(null, {
+				status: 302,
+				headers: {
+					location: 'https://github.com/ccbikai/cloudflare-worker-image'
+				}
+			});
+		}
+
+		// 白名单检查
+		if (!inWhiteList(env, url)) {
+			return new Response(null, {
+				status: 403,
+			});
+		}
+
+		// 目标图片获取与检查
+		const imageRes = await fetch(url, {headers: request.headers});
+		if (!imageRes.ok) {
+			return imageRes;
+		}
+
+		const imageBytes = new Uint8Array(await imageRes.arrayBuffer());
+		try {
+			// 图片处理
+			const photonInstance = new WebAssembly.Instance(PHOTON_WASM, {
+				'./photon_rs_bg.js': PHOTON_WASM_JS,
+			});
+			photon.setWasm(photonInstance.exports); // need patch
+			const inputImage = photon.PhotonImage.new_from_byteslice(imageBytes);
+
+			/** pipe
+				 * `resize!800,400,1|watermark!https%3A%2F%2Fmt.ci%2Flogo.png,10,10,10,10`
+				 */
+			const pipe = action.split('|')
+			const outputImage = await pipe.reduce(async (result, pipeAction) => {
+				result = await result;
+				return (await processImage(env, request, result, pipeAction)) || result;
+			}, inputImage);
+
+			// 图片编码
+			let outputImageData;
+			if (format === 'jpeg' || format === 'jpg') {
+				await initJpegWasm(JPEG_ENC_WASM);
+				outputImageData = await encodeJpeg(outputImage.get_image_data(), { quality });
+			} else if (format === 'png') {
+				await initPngWasm(PNG_ENC_WASM);
+				outputImageData = await encodePng(outputImage.get_image_data());
+			} else {
+				await initWebpWasm(WEBP_ENC_WASM);
+				outputImageData = await encodeWebp(outputImage.get_image_data(), { quality });
+			}
+
+			// 返回体构造
+			const imageResponse = new Response(outputImageData, {
+				headers: {
+					'content-type': OUTPUT_FORMATS[format],
+					'cache-control': 'public,max-age=15552000',
+				},
+			});
+
+			// 释放资源
+			inputImage.ptr && inputImage.free();
+			outputImage.ptr && outputImage.free();
+
+			// 写入缓存
+			context.waitUntil(cache.put(request, imageResponse.clone()));
+			return imageResponse;
+		} catch (error) {
+			console.error('process:error', error.name, error.message, error);
+			const errorResponse = new Response(imageBytes, {
+				headers: imageRes.headers,
+				status: 'RuntimeError' === error.name ? 415 : 500,
+			});
+			return errorResponse;
+		}
+	},
+};
